d77832907dca507a9c6c7c14c6e9a6b17d024855
mgabor <hungyg@gmail.com>
2020-02-11 19:49:03 +0100

    Rend van

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
-import time
+import sys
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
 
-import cv2
-import mss
-import numpy as np
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+from screenwatcher import ScreenWatcher
 
-mon = {'top': 10, 'left': 120, 'width': 40, 'height': 40}
 
-with mss.mss() as sct:
-    while True:
-        # im = numpy.asarray(sct.grab(mon))
-        # im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+class QCustomMainWindow(QMainWindow):
+    def __init__(self, *args):
+        super(QCustomMainWindow, self).__init__(*args)
 
-        zero = cv2.imread(r'patterns/0.png', 0)
-        w, h = zero.shape[::-1]
+        self.setWindowTitle('WolfOfStreetsOfTarkov')
+        self.text = QLabel("")
+        self.setCentralWidget(self.text)
+        self.screen_watcher = ScreenWatcher(self)
 
-        img_rgb = cv2.imread('market_cropped.png')
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        def data_cb(progress):
+            self.text.setText(progress)
 
-        # ret, img_cv = cv2.threshold(img_cv, 130, 255, cv2.THRESH_TOZERO)
-        # img_cv = cv2.bitwise_not(img_cv)
+        self.screen_watcher.progressLoad.connect(data_cb)
 
-        res = cv2.matchTemplate(img_gray, zero, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.8
-        loc = np.where(res >= threshold)
+        self.screen_watcher.start()
 
-        listings = {}
+    # def closeEvent(self, event):
+    #     self.screen_watcher.cancel()
+        # self.worker.signals.quit.emit()
+        # self.worker.quit()
+        # self.threadpool.waitForDone()
 
-        for pt in zip(*loc[::-1]):
-            print(pt)
-            listings.setdefault(pt[1] // 70, {})[pt[0] // 5] = 0  # listing row height
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-        print(listings)
-
-        # text = pytesseract.image_to_string(img_cv)
-        # print(text)
-
-        # print(pytesseract.image_to_data(img_cv))
-
-        cv2.imshow('Image', img_rgb)
-
-        # Press "q" to quit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-        # One screenshot per second
-        time.sleep(1)
+myQApplication = QApplication(sys.argv)
+myQCustomMainWindow = QCustomMainWindow(None, Qt.WindowStaysOnTopHint)
+myQCustomMainWindow.show()
+myQApplication.exec_()
--- screenwatcher.py
+++ screenwatcher.py
@@ -1,16 +1,55 @@
-import time
+import timeit
+
 import cv2
 import mss
 import numpy as np
 
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 
 
 LINE_HEIGHT = 75
 LINE_TOP_PADDING = 20
 MATCH_THRESHOLD = 0.85
+REGION = {'top': 150, 'left': 1250, 'width': 200, 'height': 830}
+
+
+def find_listings(sct, patterns):
+    time_begin = timeit.default_timer()
+
+    img_rgb = np.array(sct.grab(REGION))
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    raw_listings = {}
+    line_offset = None
+
+    for pattern in patterns:
+        res = cv2.matchTemplate(img_gray, patterns[pattern], cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= MATCH_THRESHOLD)
+
+        last_x = None
+        last_y = None
+
+        for pt in zip(*loc[::-1]):
+            if line_offset is None:
+                line_offset = pt[1] % LINE_HEIGHT - LINE_TOP_PADDING
+
+            x = pt[0]
+            y = (pt[1] + line_offset) // LINE_HEIGHT
+
+            # Numbers have to be this far from each other to filter duplicates
+            if last_x is not None and last_y is not None and abs(x - last_x) < 6 and y - last_y == 0:
+                continue
+
+            raw_listings.setdefault(y, {})[x] = pattern
+
+            last_x = x
+            last_y = y
+
+    sorted_listings = [p[1] for p in sorted(raw_listings.items())]
+    listings = [[p[1] for p in sorted(listing.items())] for listing in sorted_listings]
+
+    print("Frame time: {:>6}ms".format(round((timeit.default_timer() - time_begin) * 1000, 2)))
+    return listings
 
 
 class ScreenWatcher(QThread):
@@ -30,66 +69,13 @@ class ScreenWatcher(QThread):
         for c in {'rub', 'eur', 'usd'}:
             patterns[c] = cv2.imread(r'patterns/{}.png'.format(c), 0)
 
-        # img_rgb = cv2.imread('market_cropped.png')
-        # img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
         with mss.mss() as sct:
-            monitor = {'top': 150, 'left': 1250, 'width': 200, 'height': 830}
-
             while True:
-                img_rgb = np.array(sct.grab(monitor))
-                img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
-                raw_listings = {}
-                line_offset = None
-
-                for pattern in patterns:
-                    res = cv2.matchTemplate(img_gray, patterns[pattern], cv2.TM_CCOEFF_NORMED)
-                    loc = np.where(res >= MATCH_THRESHOLD)
-
-                    last_x = None
-                    last_y = None
-
-                    for pt in zip(*loc[::-1]):
-                        if line_offset is None:
-                            line_offset = pt[1] % LINE_HEIGHT - LINE_TOP_PADDING
-
-                        x = pt[0]
-                        y = (pt[1] + line_offset) // LINE_HEIGHT
-
-                        # Numbers have to be this far from each other to filter duplicates
-                        if last_x is not None and last_y is not None and abs(x - last_x) < 6 and y - last_y == 0:
-                            continue
-
-                        raw_listings.setdefault(y, {})[x] = pattern
-                        cv2.putText(img_rgb, str(pattern), pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (64, 128, 64))
-
-                        last_x = x
-                        last_y = y
-
-                # print(raw_listings)
-                sorted_listings = [p[1] for p in sorted(raw_listings.items())]
-                listings = [[p[1] for p in sorted(listing.items())] for listing in sorted_listings]
-                # print(sorted_listings)
-                # print(listings)
-                # print('\n'.join([r''.join([str(item) for item in listing]) for listing in listings]))
-                self.progressLoad.emit('\n'.join([r''.join([str(item) for item in listing]) for listing in listings]))
-
-                # cv2.imshow("Image", img_rgb)
-
-                # Press "q" to quit
-                # cv2.waitKey(0)
-                # if cv2.waitKey(25) & 0xFF == ord('q'):
-                #     cv2.destroyAllWindows()
-                #     break
+                self.progressLoad.emit(
+                    '\n'.join([r''.join([str(item) for item in listing]) for listing in find_listings(sct, patterns)]))
 
                 if self.wasCanceled:
                     break
-
-                # time.sleep(0.05)
-
-        # self.statusLoad.emit(True if progress == numberOfprogress else False)
-        # self.exit(0)
 
     def cancel(self):
         self.wasCanceled = True
